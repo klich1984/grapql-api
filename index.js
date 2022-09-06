714ea41e49d2e3e0808c2fbea4abc12e575d84e6
klich84 <klich84@gmail.com>
2022-09-05 22:11:22 -0500

    feat(create): querys and resolvers structured |->shema and types |-> querys and resolvers |-> custom types and arguments

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 'use strict'
 
-import { buildSchema } from 'graphql'
+// import { buildSchema } from 'graphql'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
+import { readFileSync } from 'fs'
+import path, { join } from 'path'
+import { fileURLToPath } from 'url'
+import resolvers from './lib/resolvers.js'
 // const { graphql, buildSchema } = require('graphql')
 
 const app = express()
 const port = process.env.port || 3000
 
-// schema definition
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-// resolvers config
-const resolvers = {
-  hello: () => {
-    return 'Hello World'
-  },
-}
+// schema definition and reading to file
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use(
   '/api',
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: true
   })
 )
 
