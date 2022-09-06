b5309d705ef8d3f0c31296248a2bd2b930e564bd
klich84 <klich84@gmail.com>
2022-09-05 21:02:53 -0500

    feat(config): config grapql and api |-> installation of dependencies to make the api |-> basic query creation in graphql

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict'
 
-import { graphql, buildSchema } from 'graphql'
+import { buildSchema } from 'graphql'
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
 // const { graphql, buildSchema } = require('graphql')
+
+const app = express()
+const port = process.env.port || 3000
 
 // schema definition
 const schema = buildSchema(`
@@ -10,7 +15,22 @@ const schema = buildSchema(`
   }
 `)
 
-// Run query hello
-graphql({ schema: schema, source: '{ hello }' }).then((data) =>
-  console.log(data)
+// resolvers config
+const resolvers = {
+  hello: () => {
+    return 'Hello World'
+  },
+}
+
+app.use(
+  '/api',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
 )
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
